d70d17d5103096a3ea7890136999ad75c4846cd4
AdamGreed <natanenderclon@gmail.com>
2020-07-01 23:54:25 +0300

    Что-то начинает работать.

---
+++ mysite/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from mysite import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Главная')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакты')
+
+
+@app.route('/about')
+def about_us():
+    return render_template('about.html', title='О нас')
